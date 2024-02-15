2129fa4f4b7cb81d36dd889cec86bcac63c1eea1
MD.ASSADULLA AL GALIB <135730800+assadullaalgalib@users.noreply.github.com>
2024-02-15 20:18:17 +0600

    Add files via upload

---
+++ magnets.cpp
@@ -0,0 +1,67 @@
+// https://codeforces.com/problemset/problem/344/A
+// ------------------------------------------------------------------------------
+#include<bits/stdc++.h>
+using namespace std;
+// ------------------------------------------------------------------------------
+typedef unsigned int ui;
+typedef long long int ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+// ------------------------------------------------------------------------------
+#define FST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);
+#define READ          freopen("in.txt","r",stdin)
+#define WRITE         freopen("out.txt","w",stdout);
+#define nl            "\n"
+#define PI            acos(-1.0)
+#define mem(arr,val)  memset(arr,val,sizeof(arr))
+#define pb            push_back
+#define mp            make_pair
+#define mpl           make_pair<ll,ll>
+#define pl            pair<ll,ll>
+#define all(x)        x.begin(), x.end()
+#define ff            first
+#define ss            second
+#define mod           10000007
+#define INF           2e18
+#define EPS           1e-2
+#define tt            ll t; cin>>t; while(t--)
+#define sp(n)         fixed<<setprecision(n)
+#define maxIdx(v)      max_element(all(v)) - v.begin()
+#define minIdx(v)      min_element(all(v)) - v.begin()
+#define maxEle(v)      *max_element(all(v))
+#define minEle(v)      *min_element(all(v))
+// ------------------------------------------------------------------------------
+void Galib(){
+    int n;
+    cin >> n;
+    string arr[n];
+
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int a = 1;
+    for(int i = 1; i < n; i++) {
+        if(arr[i] != arr[i - 1]) {
+            a++;
+        }
+    }
+
+    cout << a << endl;
+}
+
+int main()
+{
+    //FST_IO
+    /// Start point
+    Galib();
+
+    return 0;
+}
+
+
+
+
+
+
+
