6000ff43b7e2129da3ce22608cc519477e009e90
MD.ASSADULLA AL GALIB <135730800+assadullaalgalib@users.noreply.github.com>
2024-02-19 21:44:05 +0600

    Add files via upload

---
+++ Watermelon.cpp
@@ -0,0 +1,31 @@
+// https://codeforces.com/contest/4/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+/// Type Section
+
+
+/// Define Section
+#define nl "\n"
+
+int Galib(){
+
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0 && n != 2) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+
+}
+
+
+
+int main(){
+    /// Start point
+    Galib();
+
+    return 0;
+}
