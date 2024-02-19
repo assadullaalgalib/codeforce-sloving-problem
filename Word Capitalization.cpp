df3d1e4daf3691f009f7cbc0ea19557be539792b
MD.ASSADULLA AL GALIB <135730800+assadullaalgalib@users.noreply.github.com>
2024-02-19 21:43:03 +0600

    Add files via upload

---
+++ Word Capitalization.cpp
@@ -0,0 +1,24 @@
+//https://codeforces.com/contest/281/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+/// Type Section
+
+
+/// Define Section
+#define nl "\n"
+
+int Galib(){
+
+    string a;
+    cin >> a;
+    a[0] = toupper(a[0]);
+    cout << a << nl;
+
+}
+
+int main(){
+    /// Start point
+    Galib();
+
+    return 0;
+}
