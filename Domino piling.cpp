aa6228283d149c77977f46417b7b78d306946605
MD.ASSADULLA AL GALIB <135730800+assadullaalgalib@users.noreply.github.com>
2024-02-20 11:58:50 +0600

    Add files via upload

---
+++ Domino piling.cpp
@@ -0,0 +1,26 @@
+//https://codeforces.com/contest/50/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+/// Type Section
+
+
+/// Define Section
+#define nl "\n"
+#define FST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+int Galib(){
+
+    int a, b,c ;
+    cin >> a >> b;
+    c = (a * b) / 2;
+    cout << c << nl;
+}
+
+
+
+int main(){
+     FST_IO
+    /// Start point
+    Galib();
+
+    return 0;
+}
