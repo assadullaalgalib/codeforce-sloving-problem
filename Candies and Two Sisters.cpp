01870ec7e74dbbec2de919006333b6dd14080fa2
MD.ASSADULLA AL GALIB <135730800+assadullaalgalib@users.noreply.github.com>
2024-02-20 11:56:22 +0600

    Add files via upload

---
+++ Candies and Two Sisters.cpp
@@ -0,0 +1,33 @@
+//https://codeforces.com/contest/1335/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+
+/// Define Section
+#define nl "\n"
+#define FST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int Galib() {
+
+   long long n;
+    cin >> n;
+
+   for (int i = 0; i <n; i++) {
+     int x;
+    cin >> x;
+    long long a = (x - 1) / 2;
+
+        cout << a <<nl;
+    }
+
+}
+
+
+int main() {
+    FST_IO
+    /// Start point
+    Galib();
+
+    return 0;
+}
+
+
