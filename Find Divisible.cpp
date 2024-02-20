ce263c3841383aed916ee2aa10a7b04d86d424f5
MD.ASSADULLA AL GALIB <135730800+assadullaalgalib@users.noreply.github.com>
2024-02-20 12:01:00 +0600

    Add files via upload

---
+++ Find Divisible.cpp
@@ -0,0 +1,32 @@
+//https://codeforces.com/contest/1096/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+
+/// Define Section
+#define nl "\n"
+#define ll long long int
+#define INF 1e18
+#define FST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void Galib()
+{
+
+    int n,l, r;
+    cin >> n;
+    while(n--)
+    {
+    cin >> l >> r;
+    cout << l << " " << 2 * l << nl;
+
+}
+
+}
+
+int main()
+{
+    FST_IO
+    /// Start point
+    Galib();
+
+    return 0;
+}
