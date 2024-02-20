27897b11efeba64155f55d4627ecd8e4d68b2873
MD.ASSADULLA AL GALIB <135730800+assadullaalgalib@users.noreply.github.com>
2024-02-20 12:00:08 +0600

    Add files via upload

---
+++ Elephant.cpp
@@ -0,0 +1,26 @@
+//https://codeforces.com/contest/617/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+
+/// Define Section
+#define nl "\n"
+#define FST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void Galib() {
+
+    long long int n,a;
+   cin >> n;
+
+    a = (n+4) / 5;
+
+   cout <<a<<nl ;
+
+}
+
+int main() {
+    FST_IO
+    /// Start point
+    Galib();
+
+    return 0;
+}
