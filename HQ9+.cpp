28713e740c8ddc39eab15722134428aef09002a9
MD.ASSADULLA AL GALIB <135730800+assadullaalgalib@users.noreply.github.com>
2024-02-22 10:04:07 +0600

    Add files via upload

---
+++ HQ9+.cpp
@@ -0,0 +1,56 @@
+//https://codeforces.com/problemset/problem/133/A
+// ------------------------------------------------------------------------------
+#include<bits/stdc++.h>
+using namespace std;
+// ------------------------------------------------------------------------------
+typedef unsigned int ui;
+typedef long long int ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+// ------------------------------------------------------------------------------
+#define FST_IO ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);
+#define READ          freopen("in.txt","r",stdin)
+#define WRITE         freopen("out.txt","w",stdout);
+#define nl            "\n"
+#define PI            acos(-1.0)
+#define mem(arr,val)  memset(arr,val,sizeof(arr))
+#define pb            push_back
+#define mp            make_pair
+#define mpl           make_pair<ll,ll>
+#define pl            pair<ll,ll>
+#define all(x)        x.begin(), x.end()
+#define ff            first
+#define ss            second
+#define mod           10000007
+#define INF           2e18
+#define EPS           1e-2
+#define tt            ll t; cin>>t; while(t--)
+#define sp(n)         fixed<<setprecision(n)
+#define maxIdx(v)      max_element(all(v)) - v.begin()
+#define minIdx(v)      min_element(all(v)) - v.begin()
+#define maxEle(v)      *max_element(all(v))
+#define minEle(v)      *min_element(all(v))
+// ------------------------------------------------------------------------------
+void Galib(){
+    string s;
+    cin >> s;
+
+    for (char c : s ) {
+        if (c == 'H' || c == 'Q' || c == '9') {
+            cout << "YES" << nl;
+            return  ;
+        }
+    }
+    cout << "NO" << nl;
+
+}
+
+int main()
+{
+    //FST_IO
+    /// Start point
+    Galib();
+
+    return 0;
+}
